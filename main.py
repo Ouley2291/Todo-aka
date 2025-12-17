41dd24c74c0847b0e2c7eb88a54465d41a886e3f
Pauueu <paweu622@gmail.com>
2025-12-17 18:43:00 +0100

    Stworzenie pliku bazy

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 import customtkinter
-print(customtkinter.__version__)
+import csv
+
+
+with open('baza.csv', 'w', newline='') as csvfile:
+    fieldnames = ['Data dodania', 'Termin', 'Czynność', 'Kategoria']
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    writer.writeheader()
+    writer.writero({'Data dodania': '17.12.2025', 'Termin': '17.12.2025', 'Czynność': 'Aplikacja ToDo', 'Kategoria': 'Studia'})
+
+with open('baza.csv', 'r', newline='') as csvfile:
+    reader = csv.DictReader(csvfile)
