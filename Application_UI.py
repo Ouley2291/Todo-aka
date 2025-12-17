38227f2311d080805c8788da3ee20838e83eb573
Tatiana <herbatabezcukruuu@gmail.com>
2025-12-17 19:01:21 +0100

    uititle
    dodalam grida ktory centruje tytul

--- Application_UI.py
+++ Application_UI.py
@@ -11,12 +11,16 @@ import customtkinter
 #ustawienia aplikacji
 app=customtkinter.CTk()
 app.title("To-Do APP")
-app.geometry("600x400")
+app.geometry("1200x800")
 title_font=customtkinter.CTkFont(weight="bold",size=24)
 
+app.grid_columnconfigure(0, weight=1)
 #Przyciski etc
 title=customtkinter.CTkLabel(app,text="TO-DO APP",font=title_font)
 
+
+#miejsce
+title.grid(row=0, column=0, padx=20, pady=20)
 
 
 app.mainloop()
