923943ba17d2a708d261b2b159bd39dd5b40ca20
Oliwier <olipl2291@gmail.com>
2025-12-18 17:07:22 +0100

    walidacja daty i user input
    walidaja

--- add_task.py
+++ add_task.py
@@ -8,6 +8,7 @@ import Application_UI as app
 import csv
 import datetime
 import customtkinter
+import re
 '''
 Funkcja dodająca zadanie
 zwraca tresc zadania oraz date ukończenia zadania
@@ -17,8 +18,14 @@ Do dodania zapis do CSV
 def add_task_Ui():
     dialog=customtkinter.CTkInputDialog(text="Wpisz nazwe zadania",title="Zadanie")
     text_tresc=dialog.get_input()
-    dialog_date=customtkinter.CTkInputDialog(text="Wpisz date ukończenia zadania (w formacie XX-XX-XXXX",title="Data")
-    text_date=dialog_date.get_input()
+    text_tresc.strip()
+    while(True):
+        dialog_date=customtkinter.CTkInputDialog(text="Wpisz date ukończenia zadania (w formacie DD-MM-YYYY",title="Data")
+        text_date=dialog_date.get_input()
+        text_date.strip()
+        if re.search("((0[1-9])|1[0-9]|2[0-9]|3[0-1])-((0[1-9])|1[0-2])-[0-9]{4}",text_tresc):
+            break
+
     text_date.split("-")
     if text_tresc and text_date:
         with open('zadania.csv', mode='a', newline='', encoding='utf-8') as file:
