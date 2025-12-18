a95138a2bf1e6ee1ed418be8fb3de5c6f248ee5e
Hubert Koszyk <hubertkoszyk@zohomail.eu>
2025-12-18 15:12:51 +0100

    Update

--- add_task.py
+++ add_task.py
@@ -15,18 +15,29 @@ Do dodania zapis do CSV
 
 '''
 def add_task_Ui():
-    dialog=customtkinter.CTkInputDialog(text="Wpisz nazwe zadania")
+    dialog=customtkinter.CTkInputDialog(text="Wpisz nazwe zadania",title="Zadanie")
     text_tresc=dialog.get_input()
+<<<<<<< Updated upstream
     #NIE DZIALA NIE DA SIE DODAC DATY
     dialog_date=customtkinter.CTkInputDialog(text="Wpisz data ukończenia zadania")
+=======
+    dialog_date=customtkinter.CTkInputDialog(text="Wpisz date ukończenia zadania (w formacie XX-XX-XXXX",title="Data")
+>>>>>>> Stashed changes
     text_date=dialog_date.get_input()
+    text_date.split("-")
     if text_tresc and text_date:
-        # Otwieramy plik w trybie 'a' (append)
         with open('zadania.csv', mode='a', newline='', encoding='utf-8') as file:
             writer = csv.writer(file)
-            # Zapisujemy wiersz z danymi
             writer.writerow([text_tresc, text_date, datetime.datetime.now().strftime("%Y-%m-%d %H:%M")]) 
     return text_tresc,text_date
 
+<<<<<<< Updated upstream
 
 add_task_Ui()
+=======
+    
+
+add_task_Ui()
+
+
+>>>>>>> Stashed changes
