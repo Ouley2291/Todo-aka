f35d3f906f40a6fbc12ce7d4b604dd19ccc8c75f
Oliwier <olipl2291@gmail.com>
2025-12-18 17:32:28 +0100

    ewewq
    eweq

--- add_task.py
+++ add_task.py
@@ -18,10 +18,12 @@ Do dodania zapis do CSV
 def add_task_Ui():
     dialog=customtkinter.CTkInputDialog(text="Wpisz nazwe zadania",title="Zadanie")
     text_tresc=dialog.get_input()
+    str(text_tresc)
     text_tresc.strip()
     while(True):
         dialog_date=customtkinter.CTkInputDialog(text="Wpisz date ukończenia zadania (w formacie DD-MM-YYYY",title="Data")
         text_date=dialog_date.get_input()
+        str(text_date)
         text_date.strip()
         if re.search("((0[1-9])|1[0-9]|2[0-9]|3[0-1])-((0[1-9])|1[0-2])-[0-9]{4}",text_date):
             break
