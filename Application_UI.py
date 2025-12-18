66473f5b76853bb97cdb0e527279ac7e0bbd3813
Tatiana <herbatabezcukruuu@gmail.com>
2025-12-18 10:21:51 +0100

    ui1

--- Application_UI.py
+++ Application_UI.py
@@ -7,22 +7,94 @@ Created on Wed Dec 17 17:35:25 2025
 
 import customtkinter
 
+#kod przerobiony na klasy 
+class App(customtkinter.CTk): #moja klasa dziedziczy po klasie CTk
+    def __init__(self): #konstruktor i tam sie ustawia parametry jka tworze obiekt swojej klasy
+        super().__init__() #super odnosi sie do rodzica (czyli tu customtkinter.CTk)
+        #i ta linijka wyzej uruchamia kod startowy biblioteki, a pozniej dodaje moje ustawienia
+
+       
+        #dalsze ustawienia sa moje
+        self.title("To-Do app")
+        self.geometry("1200x800")
+        self.grid_columnconfigure((0,1), weight=1)
+
+        #tworze zmienna title_font jako definicje czcionki tytulu bo inaczej jebnie
+        self.title_font = customtkinter.CTkFont(weight="bold", size=24)
+      
+
+        #tytul
+        self.title_label=customtkinter.CTkLabel(self, text="TO-DO APP",font=self.title_font)
+        #miejsce tytulu
+        self.title_label.grid(row=0, column=0, padx=20, pady=20, columnspan=2) #columnspan = 2 przesunelo na srodek
+
+        #teraz przycisk
+        #dodajac self przycisk staje sie wlasnoscia calego obiektu, nie jest sprzatany z pamieci pozniej
+        self.button = customtkinter.CTkButton(self, text="my button", command=self.button_callback) 
+        self.button.grid(row=1, column=0, padx=20, pady=20, sticky="ew", columnspan=2) #ew to east/west wiec sie rozciagnie sie od lewej do prawej
+
+        #ckeckboxy
+        self.checkbox_1 = customtkinter.CTkCheckBox(self, text="checkbox 1")
+        self.checkbox_1.grid(row=2, column=0, padx=20, pady=(0,20), sticky="w") #w to west czyli bedzie tylko po lewej
+        self.checkbox_2 = customtkinter.CTkCheckBox(self, text="checkbox 2")
+        self.checkbox_2.grid(row=3, column=0, padx=20, pady=(0,20), sticky="w")
+
+    #funkcja dla przycisku
+    def button_callback(self):
+        print("button pressed")
+    
+app = App()
+app.mainloop()
+
+
+
+
+
+
+
+
+
+
+
+
+
+#to bylo
+"""#funkcja na przycisk - z tutorialu
+def button_callback():
+    print("button pressed")
+
 
 #ustawienia aplikacji
 app=customtkinter.CTk()
-app.title("To-Do APP")
+app.title("To-Do app")
 app.geometry("1200x800")
 title_font=customtkinter.CTkFont(weight="bold",size=24)
 
-app.grid_columnconfigure(0, weight=1)
-#Przyciski etc
-title=customtkinter.CTkLabel(app,text="TO-DO APP",font=title_font)
 
 
-#miejsce
-title.grid(row=0, column=0, padx=20, pady=20)
+#siatka
+#bez tego wszystko jest po lewej stronie 
+#wiec to "centruje" tytul i my_button
+#tupla ustawia kolumny na te sama wage, dzieki czemu checkboxy sa rowno rozlozone
+app.grid_columnconfigure((0, 1), weight=1)
 
 
-app.mainloop()
+
+
+
+#przycisk z tutorialu
+button = customtkinter.CTkButton(app, text="my_button", command=button_callback)
+button.grid(row=1, column=0, padx=20, pady=20, sticky="ew", columnspan=2) 
+#dodalam sticky i to rozszesza komorke przycisku na cale okno
+
+
+#przyciski na checkbox - zeby sprawdzic jak dzialaja
+#ustawilam je na razie po lewej stronie pod soba
+checkbox_1 = customtkinter.CTkCheckBox(app, text="checkbox 1")
+checkbox_1.grid(row=3, column=0, padx=20, pady=(0, 20), sticky="w")
+checkbox_2 = customtkinter.CTkCheckBox(app, text="checkbox 2")
+checkbox_2.grid(row=3, column=1, padx=20, pady=(0, 20), sticky="w")
+
+app.mainloop()"""
 
 
